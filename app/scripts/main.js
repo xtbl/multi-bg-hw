f2ae71742a30bc2763bf18278c0f4bba452def78
xtbl <correodecristobal@gmail.com>
2013-04-07 00:04:01 -0600

    script base

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('\'Allo \'Allo!');
+/**
+ * Investment Cycle
+ * 
+ * by Cristobal Avila
+ * 
+ */
+
+var APP = APP || {};
+
+APP.investment = (function($) {
+	//private
+	var date,
+
+	config = {
+		cycleSelector: $('#investment-cycle-selector'),
+		chkBox1: $('#chkbox-a'),
+		chkBox2: $('#chkbox-b'),
+		chkBox3: $('#chkbox-c')
+	},
+
+	init = function() {
+		console.log('init!');
+		//getMonthQuarter( monthNum )
+		var quarter;
+		for (var i = 1; i <= 12; i++) {
+			quarter = Math.ceil(i / 3);
+			console.log('month: ' + i + ' quarter: ' + quarter);
+		};
+
+	};
+
+	return {
+		init: init
+	};
+
+}(jQuery));
+
+APP.investment.init();
