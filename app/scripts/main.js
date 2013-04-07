5c6656b8eb70110364ad3067ecbdb05d2892881a
xtbl <correodecristobal@gmail.com>
2013-04-07 12:54:00 -0600

    script selectors, getquarter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,27 +10,73 @@ var APP = APP || {};
 APP.investment = (function($) {
 	//private
 	var date,
+	monthSelect = 12,	//replace with real selection
+	yearSelect,
+	selectedQuarter,
 
 	config = {
 		cycleSelector: $('#investment-cycle-selector'),
 		chkBox1: $('#chkbox-a'),
+		chkBox1Lbl: $('#lbl-chkbox-a'),
 		chkBox2: $('#chkbox-b'),
-		chkBox3: $('#chkbox-c')
+		chkBox2Lbl: $('#lbl-chkbox-b'),
+		chkBox3: $('#chkbox-c'),
+		chkBox3Lbl: $('#lbl-chkbox-c')
 	},
 
+	quarter = {
+		
+		getQuarterByMonth: function() {
+			selectedQuarter = Math.ceil(monthSelect / 3);
+			console.log('this quarter is: ' + selectedQuarter);
+		},
+		getNextInvestments: function(){
+
+
+			return 1; //returns quarter and year of the next 3 investments
+		},
+		nextInvestments: function(quart, yr) {
+		var invQ1 = 1,
+			invYear1 = 1,
+			invQ2 = 2,
+			invYear2 = 2,
+			invQ3 = 3,
+			invYear3 = 3,
+			subsequentQuarters= {
+				q1: invQ1,
+				y1: invYear1,
+				q2: invQ2,
+				y2: invYear2,
+				q3: invQ3,
+				y3: invYear3,
+			};
+
+			return subsequentQuarters;	
+		}
+	},
+	updateCheckboxes = function(){
+		config.chkBox1.attr('value',quarter.nextInvestments().q1 
+				+ '-' + quarter.nextInvestments().y1);
+		config.chkBox1Lbl.text(quarter.nextInvestments().q1 
+				+ '-' + quarter.nextInvestments().y1);
+
+		
+		console.log('updating checkboxes'+quarter.nextInvestments().y1);
+	},
+	// validate avoid more than two checks http://api.jquery.com/attr/
+	
 	init = function() {
 		console.log('init!');
 		//getMonthQuarter( monthNum )
-		var quarter;
-		for (var i = 1; i <= 12; i++) {
-			quarter = Math.ceil(i / 3);
-			console.log('month: ' + i + ' quarter: ' + quarter);
-		};
+		//setQuarterYear(month,year)
 
 	};
 
 	return {
-		init: init
+		init: init,
+		thisquarter: quarter.getQuarterByMonth,
+		nextInvestments: quarter.nextInvestments,
+		updateCheckboxes: updateCheckboxes
 	};
 
 }(jQuery));
